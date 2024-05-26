38561133ccb6d71afefea09bbc9bc0caf63e2e44
valentino <yokscraxker@gmail.com>
2024-05-26 20:58:27 -0300

    first commit

---
+++ js/custom.js
@@ -0,0 +1,44 @@
+// Get Current Year
+function getCurrentYear() {
+    var d = new Date();
+    var year = d.getFullYear();
+    document.querySelector("#displayDateYear").innerText = year;
+}
+getCurrentYear()
+
+//client section owl carousel
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    navText: [
+        '<i class="fa fa-long-arrow-left" aria-hidden="true"></i>',
+        '<i class="fa fa-long-arrow-right" aria-hidden="true"></i>'
+    ],
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        1000: {
+            items: 2
+        }
+    }
+});
+
+/** google_map js **/
+
+function myMap() {
+    const mapProp = {
+        center: { lat: -33.860664, lng: 151.208138 },
+        zoom: 14
+      };
+      const mapDiv = document.getElementById('map');
+      const map = new google.maps.Map(mapDiv, mapProp);
+      return map;
+}
